4f67c73f74477ef0c76a3e0d8ff0e9dae6578da5
Frankosviz <geom.francescopaolo@gmail.com>
2024-10-19 19:52:45 +0200

    Creating the template elements in Typescript

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+const passwordInput = document.getElementById('password') as HTMLInputElement;
+const requirementList = document.querySelectorAll('.requirement') as NodeListOf<HTMLParagraphElement>;
+const coloratedRow = document.getElementById('colorated-row') as HTMLElement;
+
